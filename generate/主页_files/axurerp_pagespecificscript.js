e2eeca055ba91cc0755e39f3187dca84bc257ac5
Scott <saintscott119@gmail.com>
2013-11-28 14:24:07 +0800

    scott's first commit

---
+++ generate/主页_files/axurerp_pagespecificscript.js
@@ -0,0 +1,16 @@
+﻿for(var i = 0; i < 13; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+
+$axure.eventManager.pageLoad(
+function (e) {
+
+});
+gv_vAlignTable['u4'] = 'top';gv_vAlignTable['u8'] = 'top';gv_vAlignTable['u10'] = 'center';gv_vAlignTable['u1'] = 'center';gv_vAlignTable['u6'] = 'top';gv_vAlignTable['u2'] = 'top';
+u11.style.cursor = 'pointer';
+$axure.eventManager.click('u11', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('页面_1.html');
+
+}
+});
